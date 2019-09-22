027f777b90e9d314bb488d455c374c32240dbcbc
hide <hide85x@gmail.com>
2019-09-22 17:27:55 +0200

    added correct scripts in pckg.json

--- docs/Projekt dla Kozy KaKo.e31bb0bc.js
+++ docs/Projekt dla Kozy KaKo.e31bb0bc.js
@@ -171,7 +171,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60236" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60587" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -347,4 +347,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/Projekt%20dla%20Kozy%20KaKo.e31bb0bc.js.map
+//# sourceMappingURL=Projekt%20dla%20Kozy%20KaKo.e31bb0bc.js.map
